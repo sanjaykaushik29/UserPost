7e3112b62fe9b0946e47fbec1f585712e04134fd
sanjaykaushik29 <sanjay@thoughtwin.com>
2023-08-09 13:10:45 +0530

    add pg

--- src/models/db.config.js
+++ src/models/db.config.js
@@ -3,23 +3,31 @@ const { Sequelize } = require('sequelize');
 const dotenv = require("dotenv")
 dotenv.config()
 
-const sequelize = new Sequelize(
-  process.env.Database_Name,
-  process.env.UserName,
-  process.env.Password,
-  {
-    host: process.env.Host,
-    dialect: process.env.Database,
-    port:5432,
-    dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false  // Only needed for self-signed certificates
-          }
-        }
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.Database_Name,
+//   process.env.UserName,
+//   process.env.Password,
+//   {
+//     host: process.env.Host,
+//     dialect: process.env.Database,
+//     port:5432,
+//     dialectOptions: {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false  // Only needed for self-signed certificates
+//           }
+//         }
+//   }
+// );
 
+const sequelize = new Sequelize('postgres://root:oihyXIsOtR8wQvl2qWrfXlv9HL9Ng1oz@dpg-cj9ilv63ttrc73ct99r0-a.oregon-postgres.render.com/ecommerce_s5xi',{
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false  // Only needed for self-signed certificates
+      }
+    }})
 
 sequelize
   .authenticate()
